f17a9064bab045a4b014974bf3a8f146a07557de
Kishore kunal <33398097+hesoyamm@users.noreply.github.com>
2020-07-21 18:48:54 +0530

    Update main.dart
    expanded widget changes to buildkey(); makes the code a lot smaller .

--- lib/main.dart
+++ lib/main.dart
@@ -8,73 +8,39 @@ class XylophoneApp extends StatelessWidget {
     final player = AudioCache();
     player.play('note$key_no.wav');
   }
-
+  Expanded buildKey({Color color, int soundNumber}){
+    return Expanded(
+      child:FlatButton(
+        color:color,
+        onPressed: (){
+          keyboard(soundNumber);
+        },
+      ),
+    );
+  }
+       
+       
   static AudioCache player = AudioCache();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor:Colors.black
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Expanded(
-                child: FlatButton(
-                  color: Colors.red,
-                  onPressed: () {
-                    keyboard(1);
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.tealAccent,
-                  onPressed: () {
-                    keyboard(2);
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.orange,
-                  onPressed: () {
-                    keyboard(3);
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.yellow,
-                  onPressed: () {
-                    keyboard(4);
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.green,
-                  onPressed: () {
-                    keyboard(5);
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.blue,
-                  onPressed: () {
-                    keyboard(6);
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.purple,
-                  onPressed: () {
-                    keyboard(7);
-                  },
-                ),
-              ),
+              buildKey(color:Colors.red, soundNumber:1),
+              buildKey(color:Colors.orange,soundNumber: 2),
+              buildKey(color:Colors.yellow,soundNumber:3),
+              buildKey(color:Colors.green,soundNumber: 4),
+              buildKey(color:Colors.teal,soundNumber: 5),
+              buildKey(color:Colors.blue,soundNumber: 6),
+              buildKey(color:Colors.purple,soundNumber: 7),
+
+              
+               
+            
             ],
           ),
         ),
